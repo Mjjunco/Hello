31c87477857a4795ffb469c7c2b7f7680702bcf3
Mjjunco <72070619+Mjjunco@users.noreply.github.com>
2020-10-03 17:20:41 +0800

    Added Csharp Github

---
+++ HelloWorldGithub/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MJ Junco
+ * Date: 03/10/2020
+ * Time: 5:18 pm
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace HelloWorldGithub
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
